0ebea949cddd3c3c5f80ed9a459ab110697aed9e
Laxman Rana <laxmanrana88@gmail.com>
2023-03-22 14:16:51 +0000

    Added creation of activities and storing in database

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -124,7 +124,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  = 'andrewbrown'
+  user_handle  = 'laxman'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)
